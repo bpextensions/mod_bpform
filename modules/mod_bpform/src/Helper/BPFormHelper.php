dd7408eaf7fc12bccf33df33d9fa1dd949b0de8b
Artur Stępień <artur.stepien@bestproject.pl>
2023-10-26 15:16:35 +0200

    Change the way module retrieves captcha solution. Changed captcha settings.

--- modules/mod_bpform/src/Helper/BPFormHelper.php
+++ modules/mod_bpform/src/Helper/BPFormHelper.php
@@ -674,7 +674,7 @@ class BPFormHelper
 
         try {
             $dispatcher->dispatch($event->getName(), $event);
-            $response = $event->getArguments();
+            $response = $event->getArgument('result');
 
         } catch (Exception $e) {
             $response = false;
